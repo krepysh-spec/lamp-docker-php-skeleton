2abcd9771544507a2a7a274d527ea1c76c192c82
Evgenii Mykhalichenko <emykhailichenko@newage.io>
2022-06-15 13:52:53 +0200

    Update app

--- src/public/index.php
+++ src/public/index.php
@@ -3,9 +3,5 @@
 use KrepyshSpec\LampDockerPhpSkeleton\Application;
 
 require_once __DIR__ . '/../vendor/autoload.php';
-require_once __DIR__ . '/../currency/vendor/autoload.php';
 
-
-echo \KrepyshSpec\World\Currency::USD;
-
-//(new Application())->start();
+(new Application())->start();
